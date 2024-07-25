d91c5adb411aa68f4b9a18481b5bfdc790e8a249
Maher Douba <maherdouba@ariya-tech.net>
2024-07-25 11:01:28 +0300

    new Pokedex-Flutter

---
+++ README.md
@@ -0,0 +1,20 @@
+# Pokedex-Flutter
+
+Pokedex demonstrates modern Flutter development with GetX, Hive, Flow, Adaptive/Responsive Design, and Material Design based on MVVM architecture.
+<br /> <br /> 
+<b>Main features:</b> <br /> 
+Offilne-first <br /> 
+Lazy-Loading(Pagination) <br /> 
+Reactive State Managment(GetX) <br /> 
+Adaptive/Responsive <br />
+
+
+
+![alt text](https://raw.githubusercontent.com/hosseinkhojany/Pokedex-Flutter/master/ARTS/desktop-list.png?raw=true)
+![alt text](https://raw.githubusercontent.com/hosseinkhojany/Pokedex-Flutter/master/ARTS/desktop.png?raw=true)
+![alt text](https://raw.githubusercontent.com/hosseinkhojany/Pokedex-Flutter/master/ARTS/mobile-land.jpg.jpg?raw=true)
+![alt text](https://raw.githubusercontent.com/hosseinkhojany/Pokedex-Flutter/master/ARTS/mobile-land.jpg?raw=true)
+![alt text](https://github.com/yalda-student/Pokedex-Flutter/blob/master/ARTS/mobile_land_list_light_mode.jpg?raw=true)
+![alt text](https://github.com/yalda-student/Pokedex-Flutter/blob/master/ARTS/mobile_port_detail_dark_mode.jpg?raw=true)
+![alt text](https://github.com/yalda-student/Pokedex-Flutter/blob/master/ARTS/mobile_port_detail_light_mode.jpg?raw=true)
+![alt text](https://github.com/yalda-student/Pokedex-Flutter/blob/master/ARTS/mobile_port_list_dark_mode.jpg?raw=true)
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:untitled1/appRouter.dart';
+import 'package:untitled1/data/config/hive/hiveConfig.dart';
+import 'package:untitled1/utils/theme_util.dart';
+
+import 'bindings/appBinding.dart';
+
+void main() async {
+  await HiveConfig.init();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]).then((_) {
+    SharedPreferences.getInstance().then((prefs) {
+      var darkModeOn = prefs.getBool('darkMode') ?? true;
+      runApp(
+        ChangeNotifierProvider<ThemeNotifier>(
+          create: (_) => ThemeNotifier(!darkModeOn ? darkTheme : lightTheme),
+          child: MyApp(
+            router: AppRouter(),
+          ),
+        ),
+      );
+    });
+  });
+
+}
+
+class MyApp extends StatelessWidget {
+  final AppRouter router;
+
+  const MyApp({Key? key, required this.router}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: themeNotifier.getTheme(),
+      // home: _buildStack()
+      initialRoute: SPLASH_ROUTE,
+      initialBinding: AppBinding(),
+      onGenerateRoute: router.generateRoute,
+      // getPages: routes,
+    );
+  }
+}
